c86e99d80a49893194df20ce46c41d441375e0c7
Maddy Brown <56206696+madbro206@users.noreply.github.com>
2024-02-26 19:33:29 -0800

    Add files via upload

---
+++ 2024-2-26 what if uw just made their free throws (Autosaved).R
@@ -0,0 +1,148 @@
+#Use wehoop package to download WNBA or NCAAW data, and translate into format to use with BasketballAnalyzeR package
+
+#https://wehoop.sportsdataverse.org/articles/getting-started-wehoop.html
+
+# You can install using the pacman package using the following code:
+if (!requireNamespace('pacman', quietly = TRUE)){
+  install.packages('pacman')
+}
+
+pacman::p_load(wehoop, dplyr, glue, tictoc, progressr)
+
+
+#equivalent in NCAAW:
+tictoc::tic()
+progressr::with_progress({
+  wbb_pbp <- wehoop::load_wbb_pbp()
+})
+tictoc::toc()
+
+tictoc::tic()
+progressr::with_progress({
+  wbb_team_box <- wehoop::load_wbb_team_box()
+})
+tictoc::toc()
+
+tictoc::tic()
+progressr::with_progress({
+  wbb_player_box <- wehoop::load_wbb_player_box()
+})
+tictoc::toc()
+
+library(BasketballAnalyzeR)
+
+Pbox <- wbb_player_box %>%
+  group_by(season, team_location, athlete_display_name) %>%
+  summarise(GP=sum(minutes>0, na.rm=TRUE), MIN=sum(minutes, na.rm = TRUE), PTS=sum(points, na.rm = TRUE),
+    P2M=sum(field_goals_made, na.rm = TRUE)-sum(three_point_field_goals_made, na.rm = TRUE), P2A=sum(field_goals_attempted, na.rm = TRUE)-sum(three_point_field_goals_attempted, na.rm = TRUE), P2p=100*P2M/P2A,
+    P3M=sum(three_point_field_goals_made, na.rm = TRUE), P3A=sum(three_point_field_goals_attempted, na.rm = TRUE), P3p=100*P3M/P3A,
+    FTM=sum(free_throws_made, na.rm = TRUE), FTA=sum(free_throws_attempted, na.rm = TRUE), FTp=100*FTM/FTA,
+    OREB=sum(offensive_rebounds, na.rm = TRUE), DREB=sum(defensive_rebounds, na.rm = TRUE), AST=sum(assists, na.rm = TRUE),
+    TOV=sum(turnovers, na.rm = TRUE), STL=sum(steals, na.rm = TRUE), BLK=sum(blocks, na.rm = TRUE),
+    PF=sum(fouls, na.rm = TRUE)) %>%
+  rename(Season=season, Team=team_location,
+         Player=athlete_display_name) %>%
+         filter(GP>5) %>%
+  as.data.frame()
+  
+#calculate game length to calculate the amount of minutes each team played
+wbb_pbp <- wbb_pbp %>%
+  group_by(game_id) %>%
+  mutate(
+    game_qtrs = max(qtr),
+    game_length = case_when(
+      game_qtrs == 4 ~ 40,
+      game_qtrs == 5 ~ 45,
+      game_qtrs == 6 ~ 50,
+      game_qtrs == 7 ~ 55,
+      game_qtrs == 8 ~ 60,
+      TRUE ~ NA_integer_
+    )
+  )
+
+#add these minute calculations to the wbb_team_box dataframe
+wbb_team_box <- wbb_team_box %>%
+  left_join(wbb_pbp %>% select(game_id, game_length), by = "game_id") %>%
+  distinct(game_id, team_location, .keep_all = TRUE)
+
+#make sure each game has a length 
+wbb_team_box <- wbb_team_box %>%
+  mutate(
+    game_length = case_when(
+      is.na(game_length) ~ 40,  # Set to 40 if game_length is NA
+      TRUE ~ game_length  # Keep the original value if not NA
+    )
+  )
+
+  
+Tbox <- wbb_team_box %>%
+  group_by(season, team_location) %>%
+  summarise(GP=n(), 
+  			MIN=sum(game_length, na.rm = TRUE), 
+            PTS=sum(team_score, na.rm = TRUE),
+            W=sum(team_winner==TRUE), 
+            L=sum(team_winner==FALSE), 
+            P2M=sum(field_goals_made, na.rm = TRUE)-sum(three_point_field_goals_made, na.rm = TRUE), 
+            P2A=sum(field_goals_attempted, na.rm = TRUE)-sum(three_point_field_goals_attempted, na.rm = TRUE), 
+            P2p=100*P2M/P2A, 
+            P3M=sum(three_point_field_goals_made, na.rm = TRUE), 
+            P3A=sum(three_point_field_goals_attempted, na.rm = TRUE), 
+            P3p=100*P3M/P3A, 
+            FTM=sum(free_throws_made, na.rm = TRUE), 
+            FTA=sum(free_throws_attempted, na.rm = TRUE), 
+            FTp=100*FTM/FTA, 
+            OREB=sum(offensive_rebounds, na.rm = TRUE), 
+            DREB=sum(defensive_rebounds, na.rm = TRUE), 
+            AST=sum(assists, na.rm = TRUE), 
+            TOV=sum(turnovers, na.rm = TRUE), 
+            STL=sum(steals, na.rm = TRUE), 
+            BLK=sum(blocks, na.rm = TRUE), 
+            PF=sum(fouls, na.rm = TRUE), 
+            PM=sum(team_score-opponent_team_score, na.rm=TRUE)) %>%
+  rename(Season=season, Team=team_location) %>%
+  as.data.frame()
+  
+
+Obox <- wbb_team_box %>%
+  group_by(season, opponent_team_location) %>%
+  summarise(GP=n(), 
+  			MIN=sum(game_length, na.rm = TRUE),
+            PTS=sum(team_score, na.rm = TRUE),
+            W=sum(team_winner==TRUE), 
+            L=sum(team_winner==FALSE), 
+            P2M=sum(field_goals_made, na.rm = TRUE)-sum(three_point_field_goals_made, na.rm = TRUE), 
+            P2A=sum(field_goals_attempted, na.rm = TRUE)-sum(three_point_field_goals_attempted, na.rm = TRUE), 
+            P2p=100*P2M/P2A, 
+            P3M=sum(three_point_field_goals_made, na.rm = TRUE), 
+            P3A=sum(three_point_field_goals_attempted, na.rm = TRUE), 
+            P3p=100*P3M/P3A, 
+            FTM=sum(free_throws_made, na.rm = TRUE), 
+            FTA=sum(free_throws_attempted, na.rm = TRUE), 
+            FTp=100*FTM/FTA, 
+            OREB=sum(offensive_rebounds, na.rm = TRUE), 
+            DREB=sum(defensive_rebounds, na.rm = TRUE), 
+            AST=sum(assists, na.rm = TRUE), 
+            TOV=sum(turnovers, na.rm = TRUE), 
+            STL=sum(steals, na.rm = TRUE), 
+            BLK=sum(blocks, na.rm = TRUE), 
+            PF=sum(fouls, na.rm = TRUE), 
+            PM=sum(team_score-opponent_team_score, na.rm=TRUE)) %>%
+  rename(Season=season, Team=opponent_team_location) %>%
+  as.data.frame()
+  
+  
+#uw <- subset(wbb_pbp, (home_team_name=="Washington" | away_team_name=="Washington") & (qtr==1 | qtr==2 | qtr==3 | qtr==4))
+
+#uw games that they lost
+uw <- subset(wbb_team_box, team_location=="Washington" & opponent_team_score > team_score)
+uw$ft_missed <- uw$free_throws_attempted-uw$free_throws_made
+uw$margin <- uw$opponent_team_score - uw$team_score
+
+uw_ft <- uw %>%
+  select(game_date, opponent_team_location, opponent_team_name, ft_missed, margin) %>%
+  mutate(ft_loss = ft_missed >= margin)
+
+mean(uw$ft_missed)
+
+#just to display overtime games
+subset(uw_ft, game_date=="2024-2-18"|game_date=="2024-2-16"|game_date=="2024-2-09")
